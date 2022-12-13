50a332880e98717747e65407e926a6dbcbb5b138
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-12-13 19:11:19 +0900

    pages

--- fontend/src/App.tsx
+++ fontend/src/App.tsx
@@ -1,31 +1,17 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import axios from "axios";
-import { useEffect, useState } from "react";
 import './App.css';
 
-const url = "http://localhost:8000"
+import Home from "./pages/home";
+import Register from "./pages/auth/register";
+import Login from "./pages/auth/login";
+import Setting from "./pages/auth/setting";
+import Greeting from "./pages/greeting";
+import About from "./pages/about";
+import NoMatch from "./pages/noMatch";
 
-const client = axios.create({
-  baseURL: "http://localhost:8000"
-});
 
-type greeting = {
-  title: string;
-};
 
 function App() {
-  const [greeting, setGreeting] = useState<greeting>({
-    title: "",
-  });
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await client.get(`${url}/api/greeting/`)
-      // console.log({ response })
-      setGreeting({ "title": response.data });
-    }
-    fetchData();
-  }, []);
 
   return (
     <div className="App">
@@ -44,6 +30,9 @@ function App() {
           <Link to="/setting">Setting</Link>
         </li>
         <li style={{ "marginRight": "10px", "listStyle": "none" }}>
+          <Link to="/greeting">Greeting</Link>
+        </li>
+        <li style={{ "marginRight": "10px", "listStyle": "none" }}>
           <Link to="/about">About</Link>
         </li>
         <li style={{ "marginRight": "10px", "listStyle": "none" }}>
@@ -52,30 +41,15 @@ function App() {
       </ul>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/greeting" element={<Greeting />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
-
-  // return (
-  //   <div className="App">
-  //     <h1>React</h1>
-  //     <h2>{greeting.title}</h2>
-  //   </div>
-  // );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function NoMatch() {
-  return <h2>このページは存在しません。</h2>;
 }
 
 export default App;
