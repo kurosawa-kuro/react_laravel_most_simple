88246fcfb27fc7db6b8ff7732aecdf77c6f20bbd
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-12-13 16:58:58 +0900

    s

--- fontend/src/App.tsx
+++ fontend/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import './App.css';
 
+const url = "http://localhost:8000"
+
+const client = axios.create({
+  baseURL: "http://localhost:8000"
+});
+
+type greeting = {
+  title: string;
+};
+
 function App() {
+  const [greeting, setGreeting] = useState<greeting>({
+    title: "",
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await client.get(`${url}/api/greeting/`)
+      // console.log({ response })
+      setGreeting({ "title": response.data });
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>React</h1>
+      <h2>{greeting.title}</h2>
     </div>
   );
 }
